0c1c9c717c73b80d0c71b7541329f46f495dc0f7
Michael Tew <mike.tew@gmail.com>
2021-03-10 23:20:13 -0800

    Changed the default value of the spinbox.

--- main.py
+++ main.py
@@ -30,13 +30,14 @@ title_label.grid(row=0, column=0, columnspan=3, pady=[0, 30])
 length_label = Label(root, text="Length:", font={"Helvetica 25"})
 length_label.grid(row=1, column=0, sticky="e")
 
-length_input = Spinbox(root, width=3, from_=1, to=10, font=("Helvetica 20"))
+length_input = Spinbox(root, width=3, from_=1, to=15, font=("Helvetica", 20))
+length_input.insert(1, 0)
 length_input.grid(row=1, column=1)
 
 generate_button = Button(root, text="Generate", font=("Helvetica 12"), command=generate_password)
 generate_button.grid(row=1, column=2, sticky="w")
 
-password_display = Entry(root, width=15, font=("Helvetica 20"))
+password_display = Entry(root, width=15, font=("Helvetica 20"), bd=0, bg="systembuttonface")
 password_display.grid(row=2, column=0, columnspan=3, pady=[30, 40])
 
 root.mainloop()
